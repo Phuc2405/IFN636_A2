9ce6022b21fbea96e6b953712254595792bd6ef3
d1mw1t <crydmwt@gmail.com>
2026-05-04 20:02:52 +1000

    Files and Frontend Structure

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://16.176.7.15:5001', // live
-  headers: { 'Content-Type': 'application/json' },
+  baseURL: "http://localhost:5001", // local
+  //baseURL: 'http://16.176.7.15:5001', // live
+  headers: { "Content-Type": "application/json" },
 });
 
 export default axiosInstance;
