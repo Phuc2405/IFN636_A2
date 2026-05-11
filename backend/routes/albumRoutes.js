4656c8374c8b466f26baa1f42cbc32c228a8305f
Peter Wu <peterwlyau@gmail.com>
2026-05-11 15:48:41 +1000

    Remove extra functions for album
    1. getAlbumById
    2. getAllAlbum
    3. createAlbum

--- backend/routes/albumRoutes.js
+++ backend/routes/albumRoutes.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  searchAlbums,
-} = require("../controllers/albumController");
+const { searchAlbums } = require("../controllers/albumController");
 
 // Search album by title
 router.get("/search", searchAlbums);
-
 
 module.exports = router;
