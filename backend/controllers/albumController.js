f67ab64cd239c06f3ddd16b74c716cc6295a324a
Peter Wu <peterwlyau@gmail.com>
2026-05-11 16:03:37 +1000

    Update albumController.js

--- backend/controllers/albumController.js
+++ backend/controllers/albumController.js
@@ -3,24 +3,6 @@ const AlbumFacade = require("../services/AlbumFacade");
 
 const Logger = require("../utils/Logger");
 
-// const searchAlbums = async (req, res) => {
-//   try {
-//     const query = req.query.q;
-//     if (!query || query.trim() === "") {
-//       return res.status(200).json([]);
-//     }
-//     const searchTerm = query.trim();
-//     const albums = await Album.find({
-//       title: { $regex: searchTerm, $options: "i" },
-//     });
-//     res.status(200).json(albums);
-//   } catch (error) {
-//     console.error("Search error:", error);
-//     res.status(500).json({ message: error.message });
-//   }
-// };
-// module.exports = { searchAlbums };
-
 const searchAlbums = async (req, res) => {
   try {
     const { q: query } = req.query;
