1b876f88b12cf63c78dd36519b81b9f2f21c1ceb
Peter Wu <peterwlyau@gmail.com>
2026-05-15 15:14:39 +1000

    Test flexible public IP address

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  //baseURL: "http://localhost:5001", // local
-  baseURL: "http://3.25.110.212", // live
+  // baseURL can be set via REACT_APP_BASE_URL env variable, defaulting to localhost for development
+  baseURL: process.env.REACT_APP_BASE_URL ? process.env.REACT_APP_BASE_URL : 'http://localhost:5001',
+  // baseURL: "http://localhost:5001", // local
+  //baseURL: 'http://16.176.7.15:5001', // live
   headers: { "Content-Type": "application/json" },
 });
 
